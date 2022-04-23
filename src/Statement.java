d67ab7745f518a99fbc0ae5ff84446997465246b
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-04-22 23:51:39 -0500

    [CLASIFICACIÓN] Funciones: Lectura de Código de arriba a abajo - Regla de Reducción.

--- src/Statement.java
+++ src/Statement.java
@@ -52,17 +52,6 @@ public class Statement {
         return formatRentalLine(rental, rentalAmount);
     }
 
-    private String formatRentalLine(Rental rental, double rentalAmount) {
-        return String.format("\t%s\t%s\n", rental.getTitle(), rentalAmount);
-    }
-
-    private int determineFrequentRenterPoints(Rental rental) {
-        boolean bonusIsEarned = (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1;
-        if (bonusIsEarned)
-            return 2;
-        return 1;
-    }
-
     private double determineAmount(Rental rental) {
         double rentalAmount = 0;
         switch (rental.getMovie().getPriceCode()) {
@@ -81,6 +70,17 @@ public class Statement {
                 break;
         }
         return rentalAmount;
+    }
+
+    private int determineFrequentRenterPoints(Rental rental) {
+        boolean bonusIsEarned = (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1;
+        if (bonusIsEarned)
+            return 2;
+        return 1;
+    }
+
+    private String formatRentalLine(Rental rental, double rentalAmount) {
+        return String.format("\t%s\t%s\n", rental.getTitle(), rentalAmount);
     }
 
     private String footer() {
