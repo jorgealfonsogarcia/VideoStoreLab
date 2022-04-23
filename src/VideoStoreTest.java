cf644d854d6473886e34a68466ce2cc9968d213f
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-04-22 23:02:33 -0500

    [LIMPIEZA] Pruebas Unitarias: Mantener las Pruebas Limpias.

--- src/VideoStoreTest.java
+++ src/VideoStoreTest.java
@@ -7,15 +7,27 @@ public class VideoStoreTest {
 
     private static final double DELTA = .001;
     private Statement statement;
+    private Movie newReleaseMovie1;
+    private Movie newReleaseMovie2;
+    private Movie childrensMovie;
+    private Movie regularMovie1;
+    private Movie regularMovie2;
+    private Movie regularMovie3;
 
     @Before
     public void setUp() {
         statement = new Statement("Customer");
+        newReleaseMovie1 = new Movie("New Release 1", Movie.NEW_RELEASE);
+        newReleaseMovie2 = new Movie("New Release 2", Movie.NEW_RELEASE);
+        childrensMovie = new Movie("Childrens", Movie.CHILDRENS);
+        regularMovie1 = new Movie("Regular 1", Movie.REGULAR);
+        regularMovie2 = new Movie("Regular 2", Movie.REGULAR);
+        regularMovie3 = new Movie("Regular 3", Movie.REGULAR);
     }
 
     @Test
     public void testSingleNewReleaseStatementTotals() {
-        statement.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
+        statement.addRental(new Rental(newReleaseMovie1, 3));
         statement.generate();
         assertEquals(9.0, statement.getTotal(), DELTA);
         assertEquals(2, statement.getFrequentRenterPoints());
@@ -23,8 +35,8 @@ public class VideoStoreTest {
 
     @Test
     public void testDualNewReleaseStatementTotals() {
-        statement.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
-        statement.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
+        statement.addRental(new Rental(newReleaseMovie1, 3));
+        statement.addRental(new Rental(newReleaseMovie2, 3));
         statement.generate();
         assertEquals(18.0, statement.getTotal(), DELTA);
         assertEquals(4, statement.getFrequentRenterPoints());
@@ -32,7 +44,7 @@ public class VideoStoreTest {
 
     @Test
     public void testSingleChildrensStatementTotals() {
-        statement.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
+        statement.addRental(new Rental(childrensMovie, 3));
         statement.generate();
         assertEquals(1.5, statement.getTotal(), DELTA);
         assertEquals(1, statement.getFrequentRenterPoints());
@@ -40,9 +52,9 @@ public class VideoStoreTest {
 
     @Test
     public void testMultipleRegularStatementTotals() {
-        statement.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
-        statement.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
-        statement.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+        statement.addRental(new Rental(regularMovie1, 1));
+        statement.addRental(new Rental(regularMovie2, 2));
+        statement.addRental(new Rental(regularMovie3, 3));
         statement.generate();
         assertEquals(7.5, statement.getTotal(), DELTA);
         assertEquals(3, statement.getFrequentRenterPoints());
@@ -50,15 +62,15 @@ public class VideoStoreTest {
 
     @Test
     public void testMultipleRegularStatementFormat() {
-        statement.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
-        statement.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
-        statement.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+        statement.addRental(new Rental(regularMovie1, 1));
+        statement.addRental(new Rental(regularMovie2, 2));
+        statement.addRental(new Rental(regularMovie3, 3));
 
         assertEquals(
                 "Rental Record for Customer\n" +
-                        "\tPlan 9 from Outer Space\t2.0\n" +
-                        "\t8 1/2\t2.0\n" +
-                        "\tEraserhead\t3.5\n" +
+                        "\tRegular 1\t2.0\n" +
+                        "\tRegular 2\t2.0\n" +
+                        "\tRegular 3\t3.5\n" +
                         "You owed 7.5\n" +
                         "You earned 3 frequent renter points\n",
                 statement.generate());
